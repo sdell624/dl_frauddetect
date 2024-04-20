7d49821aa7de475fb607e18f475db08a11184c13
Paul Krupski <pjkrupski@gmail.com>
2024-04-20 11:43:42 -0400

    dim bug fixed, accuracy of 91% reached with 10 epochs and batch of 64

--- code/transformer_model.py
+++ code/transformer_model.py
@@ -20,7 +20,7 @@ class Transformer(tf.keras.Model):
         self.classifier = Dense(1, activation='sigmoid')
 
     def _build_transformer_block(self):
-        x = Input(shape=(6, self.hidden_dim))
+        x = Input(shape=(6, self.hidden_dim))   #6 is feature size 
         layer_norm = LayerNormalization(epsilon=1e-6)(x)
         dropout = Dropout(self.dropout_rate)(layer_norm)
         attention = MultiHeadAttention(num_heads=self.num_heads, key_dim=self.hidden_dim)(dropout, dropout)
@@ -33,6 +33,11 @@ class Transformer(tf.keras.Model):
         return Model(inputs=x, outputs=outputs)
 
     def call(self, inputs, training=False):
+        print(inputs.shape, " is inputs")
+        inputs = tf.expand_dims(inputs, axis=-1) #expand for hidden_dim
+        inputs = tf.tile(inputs, [1, 1, self.hidden_dim])  #extend for hidden_dim
+        print(inputs.shape, " is new inputs")
+    
         x = self.embedding(inputs)
         for transformer_block in self.transformer_blocks:
             x = transformer_block(x)
@@ -58,12 +63,11 @@ X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
 X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
 y_train = tf.convert_to_tensor(y_train, dtype=tf.int8)
 y_test = tf.convert_to_tensor(y_test, dtype=tf.int8)
-print("Tensor shape:", X_train.shape)
-#print(labels)
+
 
 # Example usage:
 num_heads = 2
-hidden_dim = 32
+hidden_dim = 16
 num_layers = 2
 dropout_rate = 0.1
 
@@ -73,8 +77,7 @@ loss_fn = BinaryCrossentropy()
 accuracy_metric = BinaryAccuracy()
 
 model.compile(optimizer=optimizer, loss=loss_fn, metrics=[accuracy_metric])
-
-model.fit(X_train, y_train, batch_size=32, epochs=10, validation_split=0.2) 
+model.fit(X_train, y_train, batch_size=64, epochs=1, validation_split=0.2) 
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
 #predictions = model.predict(X_new_data)
